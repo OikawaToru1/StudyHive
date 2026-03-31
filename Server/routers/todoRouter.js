cda45b9fe28b266df6dc1def5fe32355fb34e9b6
Aniket <aniketadhikari2@gmail.com>
2026-03-31 18:10:52 +0545

    todo router demo

--- Server/routers/todoRouter.js
+++ Server/routers/todoRouter.js
@@ -2,9 +2,23 @@ import { Router } from "express";
 import {PrismaClient} from '../generated/prisma/index.js'
 
 const todoRouter = Router();
+const prisma = new PrismaClient()
 
-todoRouter.post('/create',(req,res)=>{
-    console.log(req.body, 'to create todo');
+todoRouter.post('/create',async(req,res)=>{
+    console.log(req.body,req.user, 'to create todo');
+    const username = req.body.username;
+
+    const newTodo = await prisma .todos.create({
+        data : {
+            content : req.body,
+            creator : {
+                connect : {
+                    username : username
+                }
+            }
+        }
+    })
+
     // js add todos to db when necessary !
 });
 
