b124f2648bc689ce19b46a39b102a9484875da25
Aniket <aniketadhikari2@gmail.com>
2026-04-10 15:09:11 +0545

    removed proxy for deployment

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -5,9 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      "/api": "http://localhost:8080/",
-    },
+    // proxy: {
+    //   "/api": "http://localhost:8080/",
+    // },
   },
   plugins: [react(), tailwindcss()],
 });
