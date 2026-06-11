e1cd5f7cf20fbebcb2c84809a9f698f7e5566570
Aniket <aniketadhikari2@gmail.com>
2026-06-11 07:57:55 +0545

    changed storage type from disk to memory

--- Server/middleware/multer.js
+++ Server/middleware/multer.js
@@ -1,13 +1,19 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-    destination : function (req, file, cb){
-        cb(null, './assets')
-    },
-    filename : function (req, file, cb){
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random()* 1E9)
-        cb(null, file.fieldname+ '-' + uniqueSuffix)
-    }
-})
+// Disk storage is suitable for locally hosted projects or when server has a good file directory system maintaind 
+
+// const storage = multer.diskStorage({
+//     destination : function (req, file, cb){
+//         cb(null, './assets')
+//     },
+//     filename : function (req, file, cb){
+//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random()* 1E9)
+//         cb(null, file.fieldname+ '-' + uniqueSuffix)
+//     }
+// })
+
+// memory storage is easier to deploy and handle as it stores stuffs in servers ram hahahaah
+
+const storage = multer.memoryStorage()
 
 export const upload = multer({storage : storage})
